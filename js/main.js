3b36f506b9fa8f39cdea0c01a94a2e469076c906
serhii <ice2me1989@gmail.com>
2020-01-03 19:01:37 +0200

    burger menu

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ $(document).ready(function(){       // плавное перемещение с�
 });
 
 
+function burgerMenu(selector) {
+	let menu = $(selector);
+	let button = menu.find('.burger-menu__button');
+	let links = menu.find('.burger-menu__link');
+	let overlay = menu.find('.burger-menu__overlay');
+
+	button.on('click', (e) => {
+		e.preventDefault();
+		toggleMenu();
+	});
+
+	links.on('click', () => toggleMenu());
+	overlay.on('click', () => toggleMenu());
+
+	function toggleMenu() {
+		menu.toggleClass('burger-menu_active');
+
+		if (menu.hasClass('burger-menu_active')) {
+			$('body').css('overflow', 'hidden');
+		} else {
+			$('body').css('overflow', 'visible');
+		}
+	}
+}
+
+burgerMenu('.burger-menu');
